877c6f79d110670e70c0b642725c8df8564d3617
nrivkin <noah.rivkin@students.olin.edu>
2018-10-02 16:05:38 -0400

    Implements AND, NAND, OR, NOR, and XOR

---
+++ Xor32bit.v
@@ -0,0 +1,18 @@
+module Xor32bit
+(
+output[31:0]  result,
+output        carryout,
+output        zero,
+output        overflow,
+input[31:0]   operandA,
+input[31:0]   operandB,
+input[2:0]    command
+);
+
+genvar i;
+generate
+for (i = 0; i < 32; i = i + 1) begin:
+    xor #30 orgate(result[i], operandA[i], operandB[i]);
+end
+endgenerate
+endmodule
